64e1bb4d60e02f8cc6b41c2006111f8f5f5d2645
pokraspolina <64229813+pokraspolina@users.noreply.github.com>
2020-11-06 15:37:11 +0200

    Update index3.js

--- Практика/7. Условные конструкции/index3.js
+++ Практика/7. Условные конструкции/index3.js
@@ -8,12 +8,11 @@ var x3 = 2;
 var x4 = 2;
 if (x1 == x2  && x2 == x3){
 	console.log(x4);
-} else 
-	if (x1 == x3  && x3 == x4){
-		console.log(x2);
+} else 	if (x1 == x3  && x3 == x4){
+	console.log(x2);
 	}
-	else if (x2 == x3  && x3 == x4){
-		console.log(x1);
-	} else {
-		console.log(x3);
+else if (x2 == x3  && x3 == x4){
+	console.log(x1);
+} else {
+	console.log(x3);
 	}
