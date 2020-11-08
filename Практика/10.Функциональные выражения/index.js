78a7dd3299fcf937da5ec483cf65f05ddc820088
pokraspolina <64229813+pokraspolina@users.noreply.github.com>
2020-11-08 23:40:43 +0200

    Add files via upload

---
+++ Практика/10.Функциональные выражения/index.js
@@ -0,0 +1,14 @@
+// Задание №1.
+// Есть следующее определение функции:
+// function add(x, y) {
+// return x + y;
+// }
+// Перепишете определение функции add() используя синтаксис Function
+// Expression.
+var x = prompt("give me x?");
+var y = prompt("give me y?");
+
+var add = function (x,y){
+	return x + y;
+};
+add();
