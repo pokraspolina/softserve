77eea946d16f8445fa455c480034caac41753a1e
pokraspolina <64229813+pokraspolina@users.noreply.github.com>
2020-11-12 17:15:59 +0200

    Update index2.js

--- Практика/1.ECMAScript 5. Основы/11.Замыкания/index2.js
+++ Практика/1.ECMAScript 5. Основы/11.Замыкания/index2.js
@@ -4,14 +4,31 @@
 // Сторонами прямоугольника может быть только число (тип данных Number,
 // значения других типов данных нельзя устанавливать), значение которого, строго
 // больше 0.
-function calc(a,b) {
-	if (a > 0 && b > 0) {
-		var P = Number((a + b) * 2);
-		var S = Number(a * b);
-	}
-	console.log("Периметр: " + P);
-	console.log("Площадь: " + S);
+function calc() {
+	var a = 4;
+	var b = 3;
+	var arr = [];
+		arr[0] = function (newA) {
+			if (newA > 0) {
+			a = newA;
+			}
+		};
+		arr[1] = function (newB) {
+			if (newB > 0) {
+			b = newB;
+			}
+		};
+		arr[2] = function () {
+			return (a + b) * 2;
+		};
+		arr[3] = function () {
+			return a * b;
+		};
+	return arr;
 }
+var calcP = calc();
+calcP[0](9); //подаю на вход в первій индекс 9
+calcP[1](3); 
+console.log(calcP[2]());
+console.log(calcP[3]());
 
-
-calc(-3,5);
