c824d4779fb823ea5a09691c9f34fcbc3b58aca5
pokraspolina <64229813+pokraspolina@users.noreply.github.com>
2020-11-26 13:59:05 +0200

    Add files via upload

---
+++ Практика/2.ECMAScript 5. Объектно-ориентированное программирование/4.Конструкторы объектов/1.js
@@ -0,0 +1,31 @@
+// Задание №1.
+// Создайте тип объектов MP3 при помощи конструкторов объектов.
+function Music (name, volume, currentMusic, status) {
+	this.name = name;
+	this.volume = volume;
+    this.currentMusic = currentMusic;
+    this.status = status;
+    this.on = function() {
+        this.status;
+        console.log("Текущий статус  MP3 " + this.name + " - " + this.status);
+    };
+    this.increaseVolume = function() {
+        this.volume++;
+        console.log("Текущий уровень музыки " + this.volume);
+    };
+    this.decreaseVolume = function() {
+            this.volume--;
+            console.log("Текущий уровень музыки " + this.volume);
+        },
+    this.nextMusic = function() {
+        	this.currentMusic++;
+        	console.log("Сейчас играет трек № " + this.currentMusic);
+        },
+    this.prevMusic = function() {
+        	this.currentMusic--;
+        console.log("Сейчас играет трек № " + this.currentMusic);
+        };
+}
+
+var lg = new Music("LG", 5, 3, true);
+// var samsung = new Music("Samsung", 1, 1, false);
