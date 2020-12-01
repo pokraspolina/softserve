5dc5a923e02628ac4c12b202b937725d27003c03
pokraspolina <64229813+pokraspolina@users.noreply.github.com>
2020-12-01 17:37:26 +0200

    Update 3.js

--- Практика/2.ECMAScript 5. Объектно-ориентированное программирование/6.Привязка контекста/3.js
+++ Практика/2.ECMAScript 5. Объектно-ориентированное программирование/6.Привязка контекста/3.js
@@ -1,7 +1,10 @@
 "use strict"
 //Напишите аналог метода bind():
 function bind(func, context) {
-   func = func(user);
+	var s = function(){
+		func.call(context);//"копия" контекста, вместо this идет контекст
+	}
+	return s;
 }
 function func() {
    console.log(this.name + " - "+ this.age);
@@ -10,6 +13,5 @@ var user = {
    name: "Tom",
    age: 20
 };
-
 var f = bind(func, user);
 f(); // "Tom – 20"
