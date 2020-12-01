cf888242a975e25026212a8bbb92f117c0326e44
pokraspolina <64229813+pokraspolina@users.noreply.github.com>
2020-12-01 17:36:27 +0200

    Update 1.js

--- Практика/2.ECMAScript 5. Объектно-ориентированное программирование/5.Функция как объект/1.js
+++ Практика/2.ECMAScript 5. Объектно-ориентированное программирование/5.Функция как объект/1.js
@@ -2,8 +2,25 @@
 /*Напишите функцию mul(), которая принимает любое количество 
 параметров разного типа и возвращает произведение параметров типа Number.
 Если параметры типа Number отсутствуют, возвращает число 0.*/
+
 function mul() {
-   // Ваш код
+    var sum = 1;
+    var calc = false;
+    for (var i = 0; i < arguments.length; i++) {
+        if (typeof arguments[i] === "number") {
+            sum = sum * arguments[i];
+            calc = true;
+        }
+    };
+    if (calc == true){
+      return sum;
+    } else {
+      return 0;
+    }
+    
 }
+
 console.log(mul(1, "str", 2, 3, true)); // 6
 console.log(mul(null, "str", false, true)); // 0
+
+//!!!!тут задание на использование arguments!!!
