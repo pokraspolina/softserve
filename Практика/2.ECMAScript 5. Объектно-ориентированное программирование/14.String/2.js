1c72d82634c80297fb3d7096d2a49ca82d3d6660
pokraspolina <64229813+pokraspolina@users.noreply.github.com>
2020-12-12 13:44:38 +0200

    Add files via upload

--- Практика/2.ECMAScript 5. Объектно-ориентированное программирование/14.String/2.js
+++ Практика/2.ECMAScript 5. Объектно-ориентированное программирование/14.String/2.js
@@ -4,6 +4,24 @@
 когда пользователь вводит строку end. Далее программа
 проверяет, есть ли в строке цифры, если да, добавляет
 в начало строки текст "Numbers: ",
-если нет – "No numbers: "
-и выводит результат в виде следующей таблицы:*/
+если нет – "No numbers: "*/
 
+/*строки 2: твоя программа запрашивает у пользователя строки
+, пока пользователь не введет строку "end". После этого
+программа начинает анализировать каждую введенную строку и выводит результат*/
+let arr = [];
+let value;
+while(true){
+	value = prompt("Введите что-то");
+	if (value == "end") {
+		break;
+	} else {
+				arr[arr.length] = value;
+	}
+	
+}
+function condition(value) {
+	 return value = Number(value);
+	}
+console.log(arr);
+console.log("Numbers:", arr.filter(condition)); 
