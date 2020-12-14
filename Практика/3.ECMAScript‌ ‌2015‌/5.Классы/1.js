57d2e133c7b66af89b16461e5cdb8d191f3c3a3b
pokraspolina <64229813+pokraspolina@users.noreply.github.com>
2020-12-14 16:44:27 +0200

    Add files via upload

--- Практика/3.ECMAScript‌ ‌2015‌/5.Классы/1.js
+++ Практика/3.ECMAScript‌ ‌2015‌/5.Классы/1.js
@@ -9,27 +9,27 @@ class Music {
    }
     on() {
         this.status = true;
-        console.log("Текущий статус MP3 " + this.name + " - " + this.status);
+        return "Текущий статус MP3 " + this.name + " - " + this.status;
     };
     increaseVolume() {
         this.volume++;
-        console.log("Текущий уровень музыки " + this.volume);
+        return "Текущий уровень музыки " + this.volume;
     };
     decreaseVolume() {
         this.volume--;
-        console.log("Текущий уровень музыки " + this.volume);
+        return "Текущий уровень музыки " + this.volume;
     };
     nextMusic() {
         this.currentMusic++;
-        console.log("Сейчас играет трек № " + this.currentMusic);
+        return "Сейчас играет трек № " + this.currentMusic;
     };
     prevMusic() {
         this.currentMusic--;
-        console.log("Сейчас играет трек № " + this.currentMusic);
+        return "Сейчас играет трек № " + this.currentMusic;
     };
 }
 
 let lg = new Music("LG", 5, 3, true);
-lg.on();
-lg.increaseVolume();
-lg.nextMusic();
+console.log(lg.on());
+console.log(lg.increaseVolume());
+console.log(lg.nextMusic());
--- Практика/3.ECMAScript‌ ‌2015‌/5.Классы/4.js
+++ Практика/3.ECMAScript‌ ‌2015‌/5.Классы/4.js
@@ -7,20 +7,45 @@
 3.	Определите статический метод, который принимает радиус и возвращает
 	длину окружности для заданного радиуса (L = 2 * π * R);
 4.	Определите метод получения объекта-круга, который возвращает копию текущего объекта;
-5.	Определите статический метод, который принимает координаты центра круг, его радиус и возвращает объект круга с заданными параметрами;
+5.	Определите статический метод, который принимает
+	координаты центра круга, его радиус и возвращает
+	объект круга с заданными параметрами;
 6.	Определите метод проверки попадания точки в круг;
 7.	Определите метод преобразования текущего состояния объекта в символьную строку (toString()).
 
 */
 class Circle{
-	constructor(center, radius){
-		this.center = center;
+	constructor(centerX, centerY, radius){
+//1
+		this.centerX = centerX;
+		this.centerY = centerY;
 		this.radius = radius;
+		this.dotX;
+		this.dotY;
 	};
-	lenght(){
+//2
+	lenght(radius){
 		return 2 * this.radius * Math.PI;
 	};
-	static lench(radius){
-		return new Circle(radius);
+//3
+	static lenghtStat(radius){
+		return 2 * radius * Math.PI;
+	};
+//4
+	copy(){
+		return new Circle();
+	};
+//5
+	static  setter (centerX, centerY, radius){
+		return new Circle(centerX, centerY, radius);
+	};
+//6
+	checker (dotX, dotY){
+		if(dotX > this.centerX && dotY > this.centerY) {
+			let a = dotX - this.centerX;
+			let b = dotY - this.centerY;
+			let c = Math.sqrt(Math.pow(a) + Math.pow(b));
+		};
+
 	};
 };
